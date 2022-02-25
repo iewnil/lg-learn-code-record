bdde49e7a7f4f76135147f728af6179597a10608
林伟 <lin.wei126@iwhalecloud.com>
2022-02-25 10:31:08 +0800

    feat: 使用redux-actions(非中间件)，简化action以及 reducer 的定义

--- 3-react/learn-redux/src/react-redux-example/actions/Counter.action.js
+++ 3-react/learn-redux/src/react-redux-example/actions/Counter.action.js
@@ -1,22 +1,35 @@
 import { DECREMENT, INCREMENT, INCREMENT_ASYNC } from "../constant/Counter.constant";
 
-export const increment = () => ({
-  type: INCREMENT
-})
 
-export const decrement = () => ({
-  type: DECREMENT
-})
+/**
+ * 普通方式定义 action
+ */
+// export const increment = () => ({
+//   type: INCREMENT
+// })
 
-// 异步 action，用于 thunk 中间件的编写形式
-// // 存在问题：action 是一个函数，异步操作写在 action 中，导致 action 不像之前单纯对象那么纯粹了
-// export const increment_async = (payload) => (dispatch) => {
-//   setTimeout(() => {
-//     dispatch(increment(payload));
-//   }, 2000);
-// }
+// export const decrement = () => ({
+//   type: DECREMENT
+// })
 
-// 异步 action，使用 redux-saga 后，和同步 action 一样简洁了
-export const increment_async = () => ({
-  type: INCREMENT_ASYNC
-})
+// // 异步 action，用于 thunk 中间件的编写形式
+// // // 存在问题：action 是一个函数，异步操作写在 action 中，导致 action 不像之前单纯对象那么纯粹了
+// // export const increment_async = (payload) => (dispatch) => {
+// //   setTimeout(() => {
+// //     dispatch(increment(payload));
+// //   }, 2000);
+// // }
+
+// // 异步 action，使用 redux-saga 后，和同步 action 一样简洁了
+// export const increment_async = () => ({
+//   type: INCREMENT_ASYNC
+// })
+
+/**
+ * 借助 redux-actions，简化 action 定义
+ */
+import { createAction } from 'redux-actions';
+
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+export const increment_async = createAction(INCREMENT_ASYNC);
--- 3-react/learn-redux/src/react-redux-example/reducers/Counter.reducer.js
+++ 3-react/learn-redux/src/react-redux-example/reducers/Counter.reducer.js
@@ -1,19 +1,42 @@
-import { DECREMENT, INCREMENT } from "../constant/Counter.constant";
+
+/**
+ * 普通方式定义 reducer
+ */
+// import { DECREMENT, INCREMENT } from "../constant/Counter.constant";
+
+// const initialState = {
+//   count: 0,
+// }
+
+// const counterReducer = (state = initialState, action) => {
+//   const { type } = action;
+//   switch (type) {
+//     case INCREMENT:
+//       return {...state, count: state.count + 1};
+//     case DECREMENT:
+//       return {...state, count: state.count - 1};
+//     default:
+//       return {...state};
+//   }
+// }
+
+// export default counterReducer;
+
+
+/**
+ * 借助 redux-actions， 简化 reducer 定义
+ */
+import { handleActions } from 'redux-actions';
+import { decrement, increment } from '../actions/Counter.action';
 
 const initialState = {
   count: 0,
 }
 
-const reducer = (state = initialState, action) => {
-  const { type } = action;
-  switch (type) {
-    case INCREMENT:
-      return {...state, count: state.count + 1};
-    case DECREMENT:
-      return {...state, count: state.count - 1};
-    default:
-      return {...state};
-  }
-}
+const counterReducer = handleActions({
+  // 传入 action，这样就简化了同时在 reducer 和 action 都需要引入常量 type 的操作（其实也可以传入常量 type）
+  [increment]: (state, action) => ({...state, count: state.count + 1}),
+  [decrement]: (state, action) => ({...state, count: state.count - 1})
+}, initialState)
 
-export default reducer;
+export default counterReducer;
